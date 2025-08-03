51bc593e3c38684ec624365237e6a4ff7e6f6c92
simplest-ayush <ujjwal301004@gmail.com>
2025-08-04 00:46:30 +0530

    removed some bugs from app.js

--- backend/app.js
+++ backend/app.js
@@ -6,12 +6,6 @@ import cookieParser from 'cookie-parser'
 const __dirname = path.resolve();
 
 const app = express();
-// Serve React build files
-app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
-});
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
@@ -33,4 +27,39 @@ app.use('/api/v1/users', userRouter)
 app.use('/api/v1/posts', postRouter)
 
 
+// Serve React build files
+app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
+
+app.get(/^\/(?!api).*/, (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+});
+
+
 export { app }
+
+// import express from 'express'
+// import cors from 'cors'
+// import cookieParser from 'cookie-parser'
+
+// const app = express();
+// app.use(cors({
+//     // origin: process.env.CORS_ORIGIN,
+//     origin: 'http://localhost:5173',
+//     credentials: true
+// }));
+
+// app.use(express.json({ limit: "16kb" }));
+// app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+// app.use(express.static("public"))
+// app.use(cookieParser())
+
+// // importing routes
+// import userRouter from './routes/user.route.js'
+// import postRouter from './routes/post.route.js'
+
+// // routes declaration
+// app.use('/api/v1/users', userRouter)
+// app.use('/api/v1/posts', postRouter)
+
+
+// export { app }
