7b272427cb62de3310f5b9a71c27ee9a3f56ca2a
simplest-ayush <ujjwal301004@gmail.com>
2025-08-04 02:45:35 +0530

    added base url directly to axios

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-    baseURL: process.env.VITE_API_URL,
+    baseURL: "https://linkedin-mini-sqz1.onrender.com/api/v1",
     withCredentials: true
 });
 export default instance;
