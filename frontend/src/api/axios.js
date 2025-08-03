483e53a8ea1a17a5bd97b45109c4f73692101525
simplest-ayush <ujjwal301004@gmail.com>
2025-08-04 01:43:36 +0530

    added base url in axios config

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-    baseURL: "/api/v1",
+    baseURL: process.env.VITE_API_URL,
     withCredentials: true
 });
 export default instance;
