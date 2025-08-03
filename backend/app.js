6ad29a51089bececd1341698c4c3c6aa6a0e9494
simplest-ayush <ujjwal301004@gmail.com>
2025-08-04 02:56:15 +0530

    removed the slash from url

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(cors({
     // origin: process.env.CORS_ORIGIN,
-    origin: 'https://linkednmini.netlify.app/',
+    origin: 'https://linkednmini.netlify.app',
     credentials: true
 }));
 
