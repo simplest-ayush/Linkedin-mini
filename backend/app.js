44abb11bcdaa4cddb06b13f91474d3bf38693d8b
simplest-ayush <ujjwal301004@gmail.com>
2025-08-03 23:54:09 +0530

    added colons

--- backend/app.js
+++ backend/app.js
@@ -14,8 +14,8 @@ app.get('*', (req, res) => {
 });
 
 app.use(cors({
-    // origin: process.env.CORS_ORIGIN,
-    origin: 'http://localhost:5173',
+    origin: process.env.CORS_ORIGIN,
+    // origin: 'http://localhost:5173',
     credentials: true
 }));
 
